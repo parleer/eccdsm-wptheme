8c9635a36a3e7affa57cdf1f75d0a3073d266272
Ryan Parlee <rparlee-github@flyinghippo.com>
2015-01-06 16:23:44 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -15,3 +15,10 @@ if (function_exists('register_sidebar'))
 add_theme_support('post-thumbnails');
 set_post_thumbnail_size(619, 268);
 add_image_size('right-thumb', 311, 268);
+
+
+// Disable WordPress built-in comments gobally, but allow for Disqus comments.
+function block_wp_comments() { 
+  wp_die( __('Sorry, comments are closed for this item.') ); 
+}
+add_action('pre_comment_on_post', 'block_wp_comments'); 
